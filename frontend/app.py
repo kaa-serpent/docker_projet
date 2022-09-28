add2cdba9d4ac8bbff75de00a9e9af82ac785d13
kaa-serpent <guillaumedecadoudal@gmail.com>
2022-09-28 09:15:11 +0200

    pep8

--- frontend/app.py
+++ frontend/app.py
@@ -8,6 +8,7 @@ DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+
 @st.cache
 def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
@@ -15,6 +16,7 @@ def load_data(nrows):
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
+
 
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
@@ -25,7 +27,7 @@ if st.checkbox('Show raw data'):
     st.write(data)
 
 st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0, 24))[0]
 st.bar_chart(hist_values)
 
 # Some number in the range 0-23
